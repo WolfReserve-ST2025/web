a1f9f4b95d24bd934291ddb656e466fce0f52174
Matevž Nerat <matevz.nerat@gmail.com>
2025-10-07 14:36:13 +0200

    Update BASE_URL to use environment variable

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'process.env.REACT_APP_API_URL';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'process.env.REACT_APP_API_URL/api',
 });
 
 instance.interceptors.request.use((config) => {
